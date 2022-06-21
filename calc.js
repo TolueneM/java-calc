74ed82d63262447ed738103cd16b01e6f192f4cd
TolueneM <105361331+TolueneM@users.noreply.github.com>
2022-06-21 21:47:59 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,27 @@
+
+
+// operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+//  operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+//  result
+document.write (`${number1} ${operator} ${number2} = ${result}`);
